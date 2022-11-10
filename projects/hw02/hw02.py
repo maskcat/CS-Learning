554cd1432156b88f52294471a718771edc7d59de
maskcat <919486463@qq.com>
2022-11-10 11:17:23 +0800

    hw02

--- projects/hw02/hw02.py
+++ projects/hw02/hw02.py
@@ -84,8 +84,7 @@ def summation_using_accumulate(n, term):
     True
     """
     "*** YOUR CODE HERE ***"
-    print("test1")
-
+    return accumulate(add,0,n,term)
 
 def product_using_accumulate(n, term):
     """An implementation of product using accumulate.
@@ -101,6 +100,7 @@ def product_using_accumulate(n, term):
     True
     """
     "*** YOUR CODE HERE ***"
+    return accumulate(mul,1,n,term)
 
 
 def compose1(func1, func2):
