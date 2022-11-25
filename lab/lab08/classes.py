d0bdbdccd51d32f7ac7878e5737e5896bceceaa3
maskcat <919486463@qq.com>
2022-11-25 09:07:23 +0000

    lab08 complete

--- lab/lab08/classes.py
+++ lab/lab08/classes.py
@@ -2,6 +2,7 @@
 
 import random
 
+
 class Card:
     cardtype = 'Staff'
 
@@ -22,7 +23,9 @@ class Card:
         >>> other_staff.defense
         500
         """
-        "*** YOUR CODE HERE ***"
+        self.name = name
+        self.attack = attack
+        self.defense = defense
 
     def power(self, other_card):
         """
@@ -41,8 +44,7 @@ class Card:
         >>> third_card.power(staff_member)
         50.0
         """
-        "*** YOUR CODE HERE ***"
-
+        return self.attack - other_card.defense / 2
 
     def effect(self, other_card, player, opponent):
         """
@@ -64,6 +66,7 @@ class Card:
         """
         return Card(self.name, self.attack, self.defense)
 
+
 class Player:
     def __init__(self, deck, name):
         """Initialize a Player object.
@@ -79,7 +82,9 @@ class Player:
         """
         self.deck = deck
         self.name = name
-        "*** YOUR CODE HERE ***"
+        self.hand = []
+        for i in range(5):
+            self.draw()
 
     def draw(self):
         """Draw a card from the player's deck and add it to their hand.
@@ -93,7 +98,7 @@ class Player:
         6
         """
         assert not self.deck.is_empty(), 'Deck is empty!'
-        "*** YOUR CODE HERE ***"
+        self.hand.append(self.deck.draw())
 
     def play(self, card_index):
         """Remove and return a card from the player's hand at the given index.
@@ -109,14 +114,14 @@ class Player:
         >>> len(test_player.hand)
         2
         """
-        "*** YOUR CODE HERE ***"
+        return self.hand.pop(card_index)
 
     def display_hand(self):
         """
         Display the player's current hand to the user.
         """
         print('Your hand:')
-        for card_index, displayed_card in zip(range(len(self.hand)),[str(card) for card in self.hand]):
+        for card_index, displayed_card in zip(range(len(self.hand)), [str(card) for card in self.hand]):
             indent = ' '*(5 - len(str(card_index)))
             print(card_index, indent + displayed_card)
 
@@ -129,6 +134,7 @@ class Player:
 ######################
 # Optional Questions #
 ######################
+
 
 class TutorCard(Card):
     cardtype = 'Tutor'
@@ -149,15 +155,19 @@ class TutorCard(Card):
         >>> len(player2.deck.cards) == initial_deck_length - 3
         True
         """
-        "*** YOUR CODE HERE ***"
-        #Uncomment the line below when you've finished implementing this method!
-        #print('{} discarded and re-drew 3 cards!'.format(opponent.name))
+        for i in range(3):
+            opponent.hand.pop(0)
+        for i in range(3):
+            Player.draw(opponent)
+        # Uncomment the line below when you've finished implementing this method!
+        print('{} discarded and re-drew 3 cards!'.format(opponent.name))
 
     def copy(self):
         """
         Create a copy of this card.
         """
         return TutorCard(self.name, self.attack, self.defense)
+
 
 class TACard(Card):
     cardtype = 'TA'
@@ -175,13 +185,16 @@ class TACard(Card):
         >>> other_card.defense
         300
         """
-        "*** YOUR CODE HERE ***"
+        temp = other_card.attack
+        other_card.attack = other_card.defense
+        other_card.defense = temp
 
     def copy(self):
         """
         Create a copy of this card.
         """
         return TACard(self.name, self.attack, self.defense)
+
 
 class ProfessorCard(Card):
     cardtype = 'Professor'
@@ -205,11 +218,17 @@ class ProfessorCard(Card):
         0
         """
         orig_opponent_deck_length = len(opponent.deck.cards)
-        "*** YOUR CODE HERE ***"
+        for c in player.deck.cards:
+            c.attack += other_card.attack
+            c.defense += other_card.defense
+        remove_lst = opponent.deck.cards[:]
+        for c in remove_lst:
+            if c.attack == c.defense:
+                opponent.deck.cards.remove(c)
         discarded = orig_opponent_deck_length - len(opponent.deck.cards)
         if discarded:
-            #Uncomment the line below when you've finished implementing this method!
-            #print('{} cards were discarded from {}\'s deck!'.format(discarded, opponent.name))
+            # Uncomment the line below when you've finished implementing this method!
+            print('{} cards were discarded from {}\'s deck!'.format(discarded, opponent.name))
             return
 
     def copy(self):
