1fef75954339b89568adde6ad372c06d73caa475
maskcat <fymaskcat@outlook.com>
2023-01-07 16:09:13 +0800

    lab13 q3 q4 q5

--- CS61A/lab/lab13/lab13.sql
+++ CS61A/lab/lab13/lab13.sql
@@ -15,25 +15,42 @@ where color = 'blue'
 
 
 CREATE TABLE matchmaker AS
-SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT t1.pet, t1.song, t1.color, t2.color
+from students as t1
+         join students as t2 on t1.pet = t2.pet and t1.song = t2.song and t1.time < t2.time;
 
 
 CREATE TABLE sevens AS
-SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT seven
+from students t1
+         join numbers t2 on t1.time = t2.time and t2.'7' = 'True' and t1.number = 7;
 
 
 CREATE TABLE favpets AS
-SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT pet, count(pet)
+from students
+group by pet
+order by count(pet) desc
+limit 10;
 
 
 CREATE TABLE dog AS
-SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT pet, count(pet)
+from students
+where pet = 'dog'
+group by pet;
 
 
 CREATE TABLE bluedog_agg AS
-SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT song, count(song)
+from bluedog_songs
+group by song
+order by count(song) desc;
 
 
 CREATE TABLE instructor_obedience AS
-SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT seven, instructor, count(instructor)
+from students
+where seven = '7'
+group by instructor;
 
